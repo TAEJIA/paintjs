d9adcef42289b4b87ed85942a6b1599956ed5d80
eodovo <104496188+eodovo@users.noreply.github.com>
2022-06-16 23:53:52 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-<<<<<<< HEAD
-const canvas = document.getElementById('jsCanvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
-const mode = document.getElementById('jsMode');
+const mode = document.getElementById("jsMode");
 
-canvas.height = 700;
-canvas.width = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
 
 let painting = false;
 let filling = false;
-
-ctx.strokeStyle = "#2c2c2c"; // 선 색
-ctx.lineWidth = 2.5; // 굵기
 
 function stopPainting() {
     painting = false;
@@ -22,30 +25,28 @@ function startPainting() {
     painting = true;
 }
 
-function onMouseMove(event) {//콜백함수, 이벤트리스너
+function onMouseMove(event) {// 콜백함수, 이벤트리스너
     const x = event.offsetX;
     const y = event.offsetY;
-    if (!painting) { // if문 !painting이 true가 될 때 실행되기 때문에 painting이 false가 되면 실행됨
-        ctx.beginPath()
-        ctx.moveTo(x, y)
+    if (!painting) { //IF문 !painting이 true가 될 떄 실행되기 때문에 painting이 false가 되면 실행됨.
+        ctx.beginPath();
+        ctx.moveTo(x, y);
     } else {
         ctx.lineTo(x, y);
         ctx.stroke();
     }
 }
 
-
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
-
+    ctx.fillStyle = color;
 }
 
-function handleRangeChange(event) { //console.log로 event.target.value <벨류를 찾음.
+function handleRangeChange(event) {
     const size = event.target.value;
     ctx.lineWidth = size;
 }
-
 
 function handleModeClick() {
     if (filling === true) {
@@ -57,66 +58,28 @@ function handleModeClick() {
     }
 }
 
-
-if (canvas) { //if 에 canvas가 있는 이유는 canvas가 들어가 있는지 확인
-    canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', startPainting);//그리기시작
-    canvas.addEventListener('mouseup', stopPainting);//그리기멈춤
-    canvas.addEventListener('mouseleave', stopPainting);//화면밖탈출(그리기멈춤)
-}
-
-Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
-//Array.from은 유사배열객체, html의 색상코드를 배열로 정리
-
-if (range) {
-    range.addEventListener('input', handleRangeChange);
-}
-
-
-if (mode) {
-    mode.addEventListener('click', handleModeClick);
-=======
-const canvas = document.getElementById('jsCanvas');
-const ctx = canvas.getContext('2d');
-
-canvas.height = 700;
-canvas.width = 700;
-
-let painting = false;
-
-ctx.strokeStyle = "#2c2c2c"; // 선 색
-ctx.lineWidth = 2.5; // 굵기
-
-function stopPainting() {
-    painting = false;
-}
-
-function startPainting() {
-    painting = true;
-}
-
-function onMouseMove(event) {//콜백함수, 이벤트리스너
-    const x = event.offsetX;
-    const y = event.offsetY;
-    if (!painting) { // if문 !painting이 true가 될 때 실행되기 때문에 painting이 false가 되면 실행됨
-        ctx.beginPath()
-        ctx.moveTo(x, y)
-    } else {
-        ctx.lineTo(x, y);
-        ctx.stroke();
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
+if (canvas) { //if에 canvas가 있는 이유는 canvas가 들어가 있는지 확인하기 위함
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
+Array.from(colors).forEach(color =>
+    color.addEventListener("click", handleColorClick)
+);
 
+if (range) {
+    range.addEventListener("input", handleRangeChange);
+}
 
-if (canvas) { //if 에 canvas가 있는 이유는 canvas가 들어가 있는지 확인
-    canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', startPainting);//그리기시작
-    canvas.addEventListener('mouseup', stopPainting);//그리기멈춤
-    canvas.addEventListener('mouseleave', stopPainting);//화면밖탈출(그리기멈춤)
->>>>>>> 6b01e3d2cc3a88743f71439282deda2bc454702f
+if (mode) {
+    mode.addEventListener("click", handleModeClick);
 }
